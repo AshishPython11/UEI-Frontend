a5ebf70296c312a7f389a4481a2f6580de02ce12
AshishPython11 <139869353+AshishPython11@users.noreply.github.com>
2024-09-17 11:37:42 +0530

    Implemented ui for login page

--- src/Pages/ForgotPassword/index.tsx
+++ src/Pages/ForgotPassword/index.tsx
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import emailicon from "../../assets/img/email.svg";
+import { toast } from "react-toastify";
 import TextField from "@mui/material/TextField";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
 import InputAdornment from "@mui/material/InputAdornment";
 import useApi from "../../hooks/useAPI";
-import { toast } from "react-toastify";
+import emailicon from "../../assets/img/email.svg";
+import gLogo from "../../assets/img/g-logo.svg";
+import gyansetuLogo from "../../assets/img/gyansetu-logo.svg";
+import loginImage from "../../assets/img/login-image.png";
 import { QUERY_KEYS } from "../../utils/const";
-import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import { Autoplay, Pagination } from "swiper/modules";
+import {
+  ArrowLeft,
+  BackArrowCircle
+} from "../../assets";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../assets/css/main.min.css";
+import FullScreenLoader from "../Loader/FullScreenLoader";
+
 const Forgotpassword = () => {
   const { postData } = useApi();
-  const navigator = useNavigate();
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
   const [value, setValue] = React.useState("student");
@@ -35,7 +49,7 @@ const Forgotpassword = () => {
             theme: "colored",
           });
           setTimeout(() => {
-            navigator("/");
+            navigate("/");
           }, 2000);
           setIsLoading(false);
         } else {
@@ -55,70 +69,213 @@ const Forgotpassword = () => {
       });
   };
   return (
-    <div className="login">
-      <div className="login_inner">
-        <div className="form_wrapper">
-          <div className="login_form">
-            <div className="login_form_inner">
-              <form>
-                <div className="title_wrapper">
-                  <h1 className="login_title">Forgot Password</h1>
-                  <div className="desc">
-                    We'll email you link to reset your password.
-                  </div>
-                </div>
-                <h4 className="text-success">{msg}</h4>
-                <div className="form_field_wrapper">
-                  <RadioGroup row value={value} onChange={handleChange}>
-                    <FormControlLabel
-                      value="student"
-                      control={<Radio />}
-                      label="Student"
-                    />
-                    <FormControlLabel
-                      value="admin"
-                      control={<Radio />}
-                      label="Admin"
-                    />
-                  </RadioGroup>
-                  <TextField
-                    onChange={(e) => setEmail(e.target.value)}
-                    id="input-with-icon-textfield"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">
-                          <img src={emailicon} alt="email" />
-                        </InputAdornment>
-                      ),
-                    }}
-                    placeholder="Email"
-                    variant="outlined"
-                  />
-                </div>
-                {/* <div className="form_field_wrapper forgotpass">
-                        <Link className="ato" to="/">Login</Link>
-                    </div> */}
-                <div className="d-flex justify-content-between form_field_wrapper signuplink_block1 forgotpass">
-                    <span>Already have an account?</span>{"  "}
-                  <Link className="ato signupa" to="/">
-                    <span className="signup_txt">Login</span>
-                  </Link>
-                </div>
+    // <div className="login">
+    //   <div className="login_inner">
+    //     <div className="form_wrapper">
+    //       <div className="login_form">
+    //         <div className="login_form_inner">
+    //           <form>
+    //             <div className="title_wrapper">
+    //               <h1 className="login_title">Forgot Password</h1>
+    //               <div className="desc">
+    //                 We'll email you link to reset your password.
+    //               </div>
+    //             </div>
+    //             <h4 className="text-success">{msg}</h4>
+    //             <div className="form_field_wrapper">
+    //               <RadioGroup row value={value} onChange={handleChange}>
+    //                 <FormControlLabel
+    //                   value="student"
+    //                   control={<Radio />}
+    //                   label="Student"
+    //                 />
+    //                 <FormControlLabel
+    //                   value="admin"
+    //                   control={<Radio />}
+    //                   label="Admin"
+    //                 />
+    //               </RadioGroup>
+    //               <TextField
+    //                 onChange={(e) => setEmail(e.target.value)}
+    //                 id="input-with-icon-textfield"
+    //                 InputProps={{
+    //                   startAdornment: (
+    //                     <InputAdornment position="start">
+    //                       <img src={emailicon} alt="email" />
+    //                     </InputAdornment>
+    //                   ),
+    //                 }}
+    //                 placeholder="Email"
+    //                 variant="outlined"
+    //               />
+    //             </div>
+    //             {/* <div className="form_field_wrapper forgotpass">
+    //                     <Link className="ato" to="/">Login</Link>
+    //                 </div> */}
+    //             <div className="d-flex justify-content-between form_field_wrapper signuplink_block1 forgotpass">
+    //                 <span>Already have an account?</span>{"  "}
+    //               <Link className="ato signupa" to="/">
+    //                 <span className="signup_txt">Login</span>
+    //               </Link>
+    //             </div>
 
-                <button
-                  type="submit"
-                  className="btn btn-primary"
-                  onClick={(e) => sendLink(e)}
-                  disabled={isLoading}
-                >
-                  Send Link
-                </button>
-              </form>
+    //             <button
+    //               type="submit"
+    //               className="btn btn-primary"
+    //               onClick={(e) => sendLink(e)}
+    //               disabled={isLoading}
+    //             >
+    //               Send Link
+    //             </button>
+    //           </form>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <>
+      {isLoading && <FullScreenLoader />}
+      <div className="without-login">
+        <header className="container-fluid mb-5 py-3 d-none d-lg-block">
+          <div className="row align-items-center">
+            <div className="col-6">
+              <img onClick={() => navigate('/')} src={gLogo} alt="" />
+            </div>
+            <div className="col-6">
+              <div className="d-flex justify-content-end">
+                <Link to="/signup" className="new-btn new-btn-secondary px-4">
+                  Register
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        </header>
+        <section className="container mb-5">
+          <div className="row ">
+            <div className="col-lg-6 d-none d-lg-block">
+              <Swiper
+                loop
+                autoplay={{
+                  delay: 3000,
+                  disableOnInteraction: false,
+                }}
+                pagination={{
+                  el: ".swiper-pagination",
+                }}
+                modules={[Autoplay, Pagination]}
+                className="mySwiper login-textslider"
+              >
+                <SwiperSlide>
+                  <div className="login-slider-card">
+                    <h2 className="fs-5 fw-semibold">
+                      Learn With Gyansetu A.I.
+                    </h2>
+                    <p className="fs-14">
+                      Welcome to the future of learning! Our AI-based Learning
+                      Management System (LMS) revolutionizes the way you learn,
+                      providing personalized and adaptive educational
+                      experiences tailored to your individual needs. Harness the
+                      power of artificial intelligence to make learning more
+                      efficient, engaging, and effective.
+                    </p>
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="login-slider-card">
+                    <h2 className="fs-5 fw-semibold">
+                      Learn With Gyansetu A.I.
+                    </h2>
+                    <p className="fs-14">
+                      Welcome to the future of learning! Our AI-based Learning
+                      Management System (LMS) revolutionizes the way you learn,
+                      providing personalized and adaptive educational
+                      experiences tailored to your individual needs. Harness the
+                      power of artificial intelligence to make learning more
+                      efficient, engaging, and effective.
+                    </p>
+                  </div>
+                </SwiperSlide>
+                <div className="swiper-pagination"></div>
+              </Swiper>
+              <img src={loginImage} alt="" />
+            </div>
+            <div className="col-lg-6">
+              <div className="access-card">
+                <div className="row gy-4">
+                  <div className="col-lg-12">
+                    <BackArrowCircle
+                      className="d-none d-lg-block"
+                      onClick={() => navigate("/")}
+                    />
+                    <ArrowLeft
+                      className="d-lg-none"
+                      onClick={() => navigate("/")}
+                    />
+                  </div>
+                  <div className="col-lg-12">
+                    <h1 className=" mt-4 mt-lg-0 mb-0 inter-600">
+                      Forgot Password
+                    </h1>
+                  </div>
+                  <div className="col-lg-12">
+                    <form method="" className="mb-3">
+                      <div className="mb-4">
+                        <div className="mb-4">
+                          <RadioGroup row value={value} onChange={handleChange}>
+                            <FormControlLabel
+                              value="student"
+                              control={<Radio />}
+                              label="Student"
+                            />
+                            <FormControlLabel
+                              value="admin"
+                              control={<Radio />}
+                              label="Admin"
+                            />
+                          </RadioGroup>
+                        </div>
+                        <label htmlFor="" className="form-label">
+                          Email / Phone
+                        </label>
+                        {/* <input type="text" className="form-control   h-52" autoFocus  placeholder="Enter Your Email / Phone"/> */}
+
+                        <TextField
+                          onChange={(e) => setEmail(e.target.value)}
+                          id="input-with-icon-textfield"
+                          placeholder="Email"
+                          // variant="outlined"
+                          fullWidth
+                        />
+                      </div>
+                      <button
+                        type="submit"
+                        onClick={(e) => sendLink(e)}
+                        className="new-btn btn-primary w-100 mb-4"
+                      >
+                        Send Link
+                      </button>
+                      <p className="text-center">
+                        Remember Now{" "}
+                        <Link
+                          to="/"
+                          className="fw-semibold"
+                          style={{
+                            color: "#9943EC",
+                          }}
+                        >
+                          {" "}
+                          Sign In here
+                        </Link>
+                      </p>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    </div>
+    </>
   );
 };
 
